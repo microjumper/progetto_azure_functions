cc8caed72ec09a3feacd6e8dfd6257a740ea0d18
Salvatore Sammarco <microjmpr@gmail.com>
2024-05-23 14:48:31 +0200

    update legal service type

--- types/LegalService.cs
+++ types/LegalService.cs
@@ -3,5 +3,6 @@ namespace appointment_scheduler.types;
 public class LegalService
 {
     public required string Id { get; set; }
-    public required string Name { get; set; }
+    public required string Title { get; set; }
+    public required string Duration { get; set; }
 }
