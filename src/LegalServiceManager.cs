74e74c96cf57399416d5b1296e9760dd211e7298
Salvatore Sammarco <microjmpr@gmail.com>
2024-05-26 17:24:11 +0200

    implement add and remove functionality

--- src/LegalServiceManager.cs
+++ src/LegalServiceManager.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace appointment_scheduler.functions;
 
@@ -22,6 +23,50 @@ public class LegalServiceManager
             var response = await container.GetItemQueryIterator<LegalService>(query).ReadNextAsync();
 
             return new OkObjectResult(response.ToList());
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "An unexpected error occurred.");
+            
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+    }
+
+    [Function("AddLegalService")]
+    public static async Task<IActionResult> AddEvent([HttpTrigger(AuthorizationLevel.Function, "post", Route = "legalServices/add")] HttpRequest req, FunctionContext context)
+    {
+        var logger = context.GetLogger(nameof(AddEvent));
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+        var newService = JsonConvert.DeserializeObject<LegalService>(requestBody);
+
+        try {
+            var container = CosmosClientManager.Instance.GetContainer("appointment_scheduler_db", "legal_service");
+            newService.Id = Guid.NewGuid().ToString();
+            var response = await container.CreateItemAsync(newService, new PartitionKey(newService.Id));
+
+            return new OkObjectResult(response.Resource);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e.Message);
+
+            return new StatusCodeResult(500);
+        }
+    }
+
+    [Function("RemoveLegalService")]
+    public static async Task<IActionResult> RemoveLegalService([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "legalServices/delete/{id}")] HttpRequest req, FunctionContext context, string id)
+    {
+        var logger = context.GetLogger(nameof(RemoveLegalService));
+
+        try
+        {
+            var container = CosmosClientManager.Instance.GetContainer("appointment_scheduler_db", "legal_service");
+            var response = await container.DeleteItemAsync<LegalService>(id, new PartitionKey(id));
+
+            return new OkObjectResult(response.Resource);
         }
         catch (Exception e)
         {
